f9727617908373376dbbbea26de550df05a667e1
torsina <theo.sagot.1@gmail.com>
2017-06-10 23:19:48 +0200

    com updated

--- modules/help.js
+++ modules/help.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 module.exports = {
     help: 'Plz send help!!',
-    func: (Client, msg, args, guild) => {
-        if (args[0] in Client.commands && Client.commands[args[0]].help)
+    func: (Client, msg, args, guild) => { //we get our parameters back
+        if (args[0] in Client.commands && Client.commands[args[0]].help) //if args[0] is a command and help property exist
             msg.channel.sendCode('asciidoc', `${args[0]} :: ${Client.commands[args[0]].help}`);
         else {
             let help = "";
-            for (let command in Client.commands) {
-                help += `${command} :: ${Client.commands[command].help}\n`
+            for (let command in Client.commands) { //for every command in the bot
+                help += `${command} :: ${Client.commands[command].help}\n` // n\ is a new line
             }
             msg.channel.sendCode('asciidoc', help);
         }
--- modules/reload.js
+++ modules/reload.js
@@ -2,12 +2,12 @@ module.exports = {
     help: 'Reload the commands',
     func: (client, msg, args, guild) => {
         console.time('reload');
-        if (args.length > 0){
+        if (args.length > 0){ //to reload a specific command
             client.load(args[0]);
             msg.channel.sendMessage('Command '+ args[0] + ' reloaded');
             console.timeEnd('reload');
         }
-        else {
+        else { //if not
             client.load();
             msg.channel.sendMessage('Commands reloaded.');
             console.timeEnd('reload');
